d5e353ccaa75a5e18703e04db30250ab9e18d503
m1k1e <mikle.fedorenko@gmail.com>
2020-08-31 20:56:55 +0300

    Add catalog img. Change files: index.html and settings.css. Add feature for application is hidden and view news for category

--- filter.js
+++ filter.js
@@ -1,12 +1,13 @@
 let articles = document.querySelectorAll('.news-block');
 let filter = document.querySelector('.filter');
 
-for (let article of articles) {
-  if (article.dataset.category === 'cats') {
-    article.classList.add('highlight');
-  }
-}
+filter.onchange = function () {
+  for (let article of articles) {
+    if (article.dataset.category !== filter.value) {
+      article.classList.add('hidden');
+    }else {
+        article.classList.remove('hidden');
+      }
 
-filter.onchange = function() {
-    console.log(filter.value);
-  };
+  }
+};
