06cd1e69b84635e15d2bf8d83fb1ae0f0dfcd75c
m1k1e <mikle.fedorenko@gmail.com>
2020-08-31 21:05:58 +0300

    Change condition for filter

--- filter.js
+++ filter.js
@@ -3,7 +3,7 @@ let filter = document.querySelector('.filter');
 
 filter.onchange = function () {
   for (let article of articles) {
-    if (article.dataset.category !== filter.value) {
+    if (article.dataset.category !== filter.value && filter.value !== 'all') {
       article.classList.add('hidden');
     }else {
         article.classList.remove('hidden');
