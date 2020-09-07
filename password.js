6a0c9d979f8e73c2f164153edaf06464df1e3cd4
m1k1e <mikle.fedorenko@gmail.com>
2020-09-07 14:21:47 +0300

    Add page registration and feature for show/hidden password

---
+++ password.js
@@ -0,0 +1,12 @@
+let password = document.querySelector('.password');
+let showPassword = document.querySelector('.show-password');
+
+showPassword.onchange = function () {
+  if(showPassword.checked){
+      password.type = 'text';
+    }else{
+      password.type = 'password';
+      }
+  
+};
+
